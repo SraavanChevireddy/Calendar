3911232d0aa100bcf69f5630a2e927c2ddd53022
Sraavan Chevireddy <63583829+SraavanChevireddy@users.noreply.github.com>
2022-08-13 00:24:02 +0530

    Initial Commit

---
+++ CalendarDates/CalendarDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarDatesApp.swift
+//  CalendarDates
+//
+//  Created by Sraavan Chevireddy on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CalendarDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
