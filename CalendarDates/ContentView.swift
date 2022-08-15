352c9bd387eede739b136578c3c9da81c4bc58fc
Sraavan Chevireddy <63583829+SraavanChevireddy@users.noreply.github.com>
2022-08-15 21:53:27 +0530

    Phone number and timezone

--- CalendarDates/ContentView.swift
+++ CalendarDates/ContentView.swift
@@ -6,17 +6,51 @@
 //
 
 import SwiftUI
+import iPhoneNumberField
 
 struct ContentView: View {
     
     // Send your list of dates to disable here.
     @State var date = [Date(), Date(timeIntervalSince1970: 1661279400)]
+    @State var phone = ""
+    @State var selectedTimeZone = "Eastern Standard Time"
+    
+    @State var timeZones = [
+        "Eastern Standard Time",
+        "Central Standard Time",
+        "Mountain Standard Time",
+        "Pacific Standard Time "
+    ]
+    
     var body: some View {
-        GeometryReader{ _ in
-            CustomDatePicker(disabledDates: date)
-                .frame(width: 250, height: 250)
-                .padding()
-        }
+//        GeometryReader{ _ in
+//            CustomDatePicker(disabledDates: date)
+//                .frame(width: 250, height: 250)
+//                .padding()
+//        }
+        
+        NavigationView{
+            Form{
+                Section {
+                    iPhoneNumberField("Phone", text: $phone)
+                        .flagHidden(false)
+                        .maximumDigits(10)
+                } header: {
+                    Text("Phone")
+                }
+
+                Section{
+                    Picker("Select", selection: $selectedTimeZone) {
+                        ForEach(timeZones, id: \.self){
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Time Zone*")
+                }
+            }
+            .navigationTitle("Schedule a Call")
+        }.navigationBarTitleDisplayMode(.inline)
         
     }
 }
